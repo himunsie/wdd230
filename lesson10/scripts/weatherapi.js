b414cd0405a0cbb12f9044368190598e8d9c1003
Heidi <hlmunson@hotmail.com>
2022-11-14 21:29:14 -0700

    lesson10

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,37 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=fairbanks&units=imperial&appid=8c74a60b5f6b568c8e3e1e4f679ce818';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+
+  function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc.toUpperCase();
+  }
+  
